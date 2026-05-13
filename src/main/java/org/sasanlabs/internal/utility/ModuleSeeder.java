fdbb1fc5b7149da7c0172505d9ca00c4df4058d7
aashpis <37486859+aashpis@users.noreply.github.com>
2026-05-13 14:28:58 -0400

    commenting

--- src/main/java/org/sasanlabs/internal/utility/ModuleSeeder.java
+++ src/main/java/org/sasanlabs/internal/utility/ModuleSeeder.java
@@ -1,5 +1,7 @@
 package org.sasanlabs.internal.utility;
 
+import org.sasanlabs.configuration.DatabaseSeeder;
+
 /**
  * Implement to seed the database for a module. ModuleSeeders are auto-discovered and called by
  * {@link DatabaseSeeder} on application startup.
